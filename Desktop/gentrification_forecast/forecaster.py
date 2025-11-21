a9b096b4d61ca88eacc4135d0e56b7dd3af366a3
juliaguxrra <juliaguxrra@gmail.com>
2025-11-20 21:09:14 -0500

    imports

--- Desktop/gentrification_forecast/forecaster.py
+++ Desktop/gentrification_forecast/forecaster.py
@@ -1,2 +1,9 @@
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+
+df_2024=pd.read_csv('data/2024_data.csv')
+df_2019=pd.read_csv('data/2019_data.csv')
